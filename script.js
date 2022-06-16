b9f363686d504de53ca1ddd851128157cd0e5b11
Pignataro67 <Pignataro67@gmail.com>
2022-06-16 05:06:39 -0700

    JS

--- script.js
+++ script.js
@@ -27,7 +27,17 @@ function showMovies(movies) {
       ${overview}
         </div>
       `
-        
+
     main.appendChild(movieEl)
   })
+}
+
+function getClassByRate(vote) {
+  if(vote >= 8) {
+    return 'green'  
+  } else if(vote >= 5) {
+        return 'orange'
+  } else {
+      return 'red'
+  }
 }
