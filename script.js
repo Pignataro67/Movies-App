2ee252593adf8f8b5f2cac72747a655f087846ed
Pignataro67 <Pignataro67@gmail.com>
2022-06-17 05:17:27 -0700

    JS

--- script.js
+++ script.js
@@ -52,4 +52,13 @@ function getClassByRate(vote) {
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
-}
+
+  const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
